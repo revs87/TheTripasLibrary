12330f1a1c24a030bf27ea50da0a9db4f5ac4a35
Rui Vieira <rui.s.vieira@itsector.pt>
2016-03-28 18:19:13 +0100

    + TwoDimensionGestureScrollView controller added + Utils XML object merge method added

---
+++ core/src/main/java/com/tripasfactory/thetripaslibrary/Utils/Utils.java
@@ -0,0 +1,64 @@
+package com.tripasfactory.thetripaslibrary.Utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.tripasfactory.thetripaslibrary.Base.BaseActivity;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by rvieira on 03/03/2016.
+ */
+public class Utils {
+
+    /**
+     * Generate a value suitable for use in {@link #setId(int)}.
+     * This value will not collide with ID values generated at build time by aapt for R.id.
+     *
+     * @return a generated ID value
+     */
+    private static final AtomicInteger sNextGeneratedId = new AtomicInteger(1);
+
+    public static int generateViewId() {
+        for (; ; ) {
+            final int result = sNextGeneratedId.get();
+            // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
+            int newValue = result + 1;
+            if (newValue > 0x00FFFFFF) newValue = 1; // Roll over to 1, not 0.
+            if (sNextGeneratedId.compareAndSet(result, newValue)) {
+                return result;
+            }
+        }
+    }
+
+    /*
+    * Prompts Messaging dialog
+    * */
+    public static void sendSMS(Activity activity, String smsContent) {
+        BaseActivity.pushedBackButtonBackToolbar = true;
+
+        Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+        sendIntent.setType("vnd.android-dir/mms-sms");
+        sendIntent.putExtra("sms_body", smsContent);
+        sendIntent.putExtra("compose_mode", true);
+        activity.startActivity(Intent.createChooser(sendIntent, "SMS: "));
+    }
+
+    /*
+    * Prompts Email dialog
+    * */
+    public static void sendEmail(Activity activity, String emailSubject, String emailContent) {
+        BaseActivity.pushedBackButtonBackToolbar = true;
+
+        Intent mEmail = new Intent(Intent.ACTION_SEND);
+        mEmail.putExtra(Intent.EXTRA_EMAIL, new String[]{});
+        mEmail.putExtra(Intent.EXTRA_SUBJECT, emailSubject);
+        mEmail.putExtra(Intent.EXTRA_TEXT, emailContent);
+        // prompts to choose email client
+        mEmail.setType("message/rfc822");
+        activity.startActivity(Intent.createChooser(mEmail, "Choose an email client to send your"));
+    }
+
+
+}
